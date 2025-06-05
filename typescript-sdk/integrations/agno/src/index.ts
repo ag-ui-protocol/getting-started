7cf00f621b866ad07c206b21f88bc8d48e2843e3
manu <manuhortet@gmail.com>
2025-06-05 15:54:22 +0200

    feat: update agno index.ts

--- typescript-sdk/integrations/agno/src/index.ts
+++ typescript-sdk/integrations/agno/src/index.ts
@@ -1,3 +1,10 @@
+/** 
+ * Agno is a framework for building Multi-Agent Systems with memory, knowledge and reasoning.
+ * Check more about using Agno: https://docs.agno.com/
+ *
+ * Use the following AgnoAgent class to interact with your Agno Agents and Teams using the Dojo frontend in this repository.
+ */
+
 import { HttpAgent } from "@ag-ui/client";
 
 export class AgnoAgent extends HttpAgent {}
